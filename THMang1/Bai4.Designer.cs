bd3072fdee2179182f6d447a21a963390c3b1cb0
Pham Nhat Huy <huyphmnhat@gmail.com>
2025-03-03 03:39:01 +0700

    them cau 4

--- THMang1/Bai4.Designer.cs
+++ THMang1/Bai4.Designer.cs
@@ -28,12 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBoxTo);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBoxFrom);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(112, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(563, 173);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập thông tin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn một số: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chọn:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(130, 42);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(372, 22);
+            this.txtInput.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Items.AddRange(new object[] {
+            "Decimal",
+            "Hexadecimal",
+            "Binary"});
+            this.comboBoxFrom.Location = new System.Drawing.Point(145, 76);
+            this.comboBoxFrom.Name = "comboBox2";
+            this.comboBoxFrom.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxFrom.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(297, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "sang";
+            // 
+            // comboBox3
+            // 
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Items.AddRange(new object[] {
+            "Decimal",
+            "Hexadecimal",
+            "Binary"});
+            this.comboBoxTo.Location = new System.Drawing.Point(363, 76);
+            this.comboBoxTo.Name = "comboBox3";
+            this.comboBoxTo.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxTo.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(130, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(372, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Thực hiện";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(182, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Kết quả";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(257, 259);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(400, 22);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // Bai4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Bai4";
             this.Text = "Bai4";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOutput;
     }
 }
--- THMang1/Bai4.cs
+++ THMang1/Bai4.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,124 @@ namespace THMang1
         public Bai4()
         {
             InitializeComponent();
+        }
+
+        public static string BinaryToDecimal(string binary)
+        {
+            return Convert.ToInt32(binary, 2).ToString();
+        }
+
+        public static string BinaryToHex(string binary)
+        {
+            return Convert.ToInt32(binary, 2).ToString("X");
+        }
+
+        public static string DecimalToBinary(string decimalNumber)
+        {
+            return Convert.ToString(int.Parse(decimalNumber), 2);
+        }
+
+        public static string DecimalToHex(string decimalNumber)
+        {
+            return int.Parse(decimalNumber).ToString("X");
+        }
+
+        public static string HexToBinary(string hex)
+        {
+            return Convert.ToString(Convert.ToInt32(hex, 16), 2);
+        }
+
+        public static string HexToDecimal(string hex)
+        {
+            return Convert.ToInt32(hex, 16).ToString();
+        }
+        public static bool IsValidBinary(string binary)
+        {
+            return Regex.IsMatch(binary, "^[01]+$");
+        }
+
+        public static bool IsValidDecimal(string decimalNumber)
+        {
+            return Regex.IsMatch(decimalNumber, "^[0-9]+$");
+        }
+
+        public static bool IsValidHex(string hex)
+        {
+            return Regex.IsMatch(hex, "^[0-9A-Fa-f]+$");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool IsValidBase(string s)
+        {
+            return String.Equals(s, "Binary", StringComparison.OrdinalIgnoreCase) ||
+                String.Equals(s, "Decimal", StringComparison.OrdinalIgnoreCase) ||
+                String.Equals(s, "Hexadecimal", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private int GetBase(string s)
+        {
+            if (String.Equals(s, "Binary", StringComparison.OrdinalIgnoreCase))
+            {
+                return 2;
+            }
+            else if (String.Equals(s, "Decimal", StringComparison.OrdinalIgnoreCase))
+            {
+                return 10;
+            }
+            else if (String.Equals(s, "Hexadecimal", StringComparison.OrdinalIgnoreCase))
+            {
+                return 16;
+            }
+
+            throw new Exception("Invalid base");
+        }
+
+        private string ConvertToBase(string s, int baseFrom, int baseTo)
+        {
+            // Convert from source base to decimal
+            int decimalValue = Convert.ToInt32(s, baseFrom);
+
+            // Convert from decimal to target base
+            return Convert.ToString(decimalValue, baseTo).ToUpper();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fromFormat = comboBoxFrom.SelectedItem?.ToString().ToLower();
+            var toFormat = comboBoxTo.SelectedItem?.ToString().ToLower();
+
+            if (fromFormat is null || toFormat is null)
+            {
+                MessageBox.Show("Hệ cơ số không phù hợp.");
+                txtOutput.Text = "";
+                return;
+            }
+
+            if (!IsValidBase(fromFormat) || !IsValidBase(toFormat))
+            {
+                MessageBox.Show("Hệ cơ số không phù hợp.");
+                txtOutput.Text = "";
+                return;
+            }
+            int fromBase = GetBase(fromFormat);
+            int toBase = GetBase(toFormat);
+
+            try
+            {
+                string result = ConvertToBase(txtInput.Text, fromBase, toBase);
+                txtOutput.Text = result;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Input không hợp lệ");
+                txtOutput.Text = "";
+                return;
+            }
+
         }
     }
 }
