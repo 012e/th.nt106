0b82fdf8644b260729b70bdcb84b7a3b17eba812
Pham Nhat Huy <huyphmnhat@gmail.com>
2025-03-03 02:00:49 +0700

    them bai 2

--- THMang1/Bai2.cs
+++ THMang1/Bai2.cs
@@ -16,5 +16,61 @@ namespace THMang1
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bai2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(inputSo1.Text, out double num1))
+            {
+                MessageBox.Show("Số thứ nhất không phải là số, hãy nhập lại.");
+                return;
+            }
+
+            if (!Double.TryParse(inputSo2.Text, out double num2))
+            {
+                MessageBox.Show("Số thứ hai không phải là số, hãy nhập lại.");
+                return;
+            }
+
+            if (!Double.TryParse(inputSo3.Text, out double num3))
+            {
+                MessageBox.Show("Số thứ ba không phải là số, hãy nhập lại.");
+                return;
+            }
+
+            double min = Math.Min(num1, Math.Min(num2, num3));
+            double max = Math.Max(num1, Math.Max(num2, num3));
+
+            outputMax.Text = max.ToString();
+            outputMin.Text = min.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            inputSo1.Text = "";
+            inputSo2.Text = "";
+            inputSo3.Text = "";
+            outputMax.Text = "";
+            outputMin.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
