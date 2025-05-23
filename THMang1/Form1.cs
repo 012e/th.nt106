eed9a2d50161f92bc31a43b290969cfcd67ac52f
Pham Nhat Huy <huyphmnhat@gmail.com>
2025-05-23 07:45:37 +0700

    clean up

--- THMang1/Form1.cs
+++ THMang1/Form1.cs
@@ -16,29 +16,5 @@ namespace THMang1
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Bai1 f = new Bai1();
-            f.Show();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Bai2 f = new Bai2();
-            f.Show();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Bai3 f = new Bai3();
-            f.Show();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Bai4 f = new Bai4();
-            f.Show();
-        }
     }
 }
